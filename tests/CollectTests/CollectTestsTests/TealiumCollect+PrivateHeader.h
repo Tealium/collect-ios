0822557374d45dd7a5c629b97c6c83177c9da342
George Webster <accounts@ethyreal.com>
2015-06-04 14:58:11 -0700

    Collect Library Unit Tests added

---
+++ tests/CollectTests/CollectTestsTests/TealiumCollect+PrivateHeader.h
@@ -0,0 +1,39 @@
+//
+//  TealiumCollect+PrivateHeader.h
+//  CollectTests
+//
+//  Created by George Webster on 6/4/15.
+//  Copyright (c) 2015 Tealium Inc. All rights reserved.
+//
+
+#import <TealiumCollect/TealiumCollect.h>
+#import <TealiumCollect/TEALSettingsStore.h>
+#import <TealiumCollect/TEALAudienceStreamDispatchManager.h>
+#import <TealiumCollect/TEALURLSessionManager.h>
+#import <TealiumCollect/TEALProfileStore.h>
+
+@interface TealiumCollect (Private)
+
+@property (strong, nonatomic) TEALSettingsStore *settingsStore;
+@property (strong, nonatomic) TEALOperationManager *operationManager;
+@property (strong, nonatomic) TEALProfileStore *profileStore;
+@property (strong, nonatomic) TEALAudienceStreamDispatchManager *dispatchManager;
+@property (strong, nonatomic) TEALURLSessionManager *urlSessionManager;
+
+@property (nonatomic) BOOL enabled;
+
+- (instancetype) initPrivate;
+
+- (void) setupConfiguration:(TEALCollectConfiguration *)configuration
+                 completion:(TEALBooleanCompletionBlock)setupCompletion;
+
+- (void) fetchSettings:(TEALSettings *)settings
+            completion:(TEALBooleanCompletionBlock)setupCompletion;
+
+- (void) enable;
+
+- (void) joinTraceWithToken:(NSString *)token;
+- (void) leaveTrace;
+
+@end
+
