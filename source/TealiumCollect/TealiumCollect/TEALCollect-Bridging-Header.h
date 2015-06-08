6171e4ecfa61c2af1d696ecfda855dc9cd60aade
George Webster <accounts@ethyreal.com>
2015-06-08 15:49:11 -0700

    removed source from gh-pages branch

--- source/TealiumCollect/TealiumCollect/TEALCollect-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  TEALCollect-Bridging-Header.h
-//  Tealium Collect Library
-//
-//  Created by George Webster on 3/5/15.
-//  Copyright (c) 2015 Tealium Inc. All rights reserved.
-//
-
-#import <TealiumCollect/TealiumCollect.h>
