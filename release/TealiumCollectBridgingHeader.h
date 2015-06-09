5ca0dac03b1a9cc1f89843a4739073a2b1555afc
George Webster <accounts@ethyreal.com>
2015-06-09 11:23:33 -0700

    updated theme, imported gh-pages

--- release/TealiumCollectBridgingHeader.h
+++
@@ -1,9 +0,0 @@
-//
-//  TealiumCollectBridgingHeader.h
-//  CollectAPISampler
-//
-//  Created by George Webster on 6/4/15.
-//  Copyright (c) 2015 Tealium Inc. All rights reserved.
-//
-
-#import <TealiumCollect/TealiumCollect.h>
